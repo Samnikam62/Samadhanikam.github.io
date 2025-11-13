4e98c1fd7edc4d170bebe769b6ce457673043eb3
Samadhan Nikam <nsamadhan188@gmail.com>
2025-11-13 17:12:59 +0530

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{fontFamily:"system-ui", padding:40}}>
-      <h1>Welcome — my portfolio</h1>
-      <p>Deployed with Next.js + Vercel</p>
-    </main>
-  )
-}
